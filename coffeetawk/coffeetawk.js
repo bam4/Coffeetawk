3c279ef7a6a69586486878e105df1e0d928c4057
Brett Moody <brettmoody@me.com>
2018-09-11 23:33:53 +0800

    Adding coffeetawk.js

---
+++ coffeetawk/coffeetawk.js
@@ -0,0 +1,34 @@
+//CoffeeTawk — by 'your name here' — applab, lab 1, 2018
+
+// Statement #1:
+// "I feel like we have a larger demand here in Shanghai,"
+// Assumption: The brand could get many more customers, but can't for some reason.
+
+// Statement #2:
+// "it can take up to a minute or more just to take an order, and another 5 minutes for them to walk out the door with their beverage."
+// Assumption: Explicitly stated.
+
+// Statement #3.
+// "Payment is quick, most people use AliPay or WeChat Pay."
+// Assumption: The majority of people use Alipay or WeChat pay and not something else.
+
+// Statements #4, 5, 6:
+// "he order-taking process is slow, 
+//our baristas are fast, 
+// but they struggle to keep up with making drinks and handling customer questions."
+// Assumptions: Explicitly stated.
+
+// Statement #7:
+// "and I’m sure it’s through some combination of technology and operational efficiencies"
+// Assumption: The answer is not some other solution.
+
+// Statement #8:
+// " I think most people know what they want before they even walk in the store,
+// Assumption: Explicitly stated.
+
+// Statement #9:
+// "What about the people who just want to walk in, grab their drink and go. "I think we’re losing that business.”
+// Assumption: What about people who do not want to leave?
+
+
+
