b29182db3e7cbe803eef5e4aec34c25b08670de6
Brett Moody <brettmoody@me.com>
2018-09-11 23:52:15 +0800

    More CoffeeTawk

--- coffeetawk/coffeetawk.js
+++ coffeetawk/coffeetawk.js
@@ -1,5 +1,7 @@
 //CoffeeTawk — by 'your name here' — applab, lab 1, 2018
 
+// STATEMENTS AND ASSUMPTIONS:
+
 // Statement #1:
 // "I feel like we have a larger demand here in Shanghai,"
 // Assumption: The brand could get many more customers, but can't for some reason.
@@ -31,4 +33,36 @@
 // Assumption: What about people who do not want to leave?
 
 
+// PSEUDOCODE:
 
+// START PROGRAM:
+
+// Statement #1
+// Count how many people walk by the shop.
+// Count how many enter.
+// Calculate the percentage that enters.
+
+// Statement #2
+// Calculate how long each customer is in the store.
+// Average this number.
+
+// Statement #3:
+// Calculate which percentage uses WeChat or AliPay
+
+// Statement #4, 5, 6
+// Time each of these values.
+// 
+
+
+// CODE:
+
+
+let customerDictionary = {"Fred":"AliPay", "Bill":"WeChatPay", "Amy":"Cash", "Joan":"Credit Card", "Don":"WeChatPay", "Luis":"WeChatPay",};
+
+let totalCustomers = 0;
+let weChatPayUsers = 0;
+let aliPayUsers = 0;
+
+let currentCustomer = "Fred";
+
+let customers = {["Fred", "String"] }
